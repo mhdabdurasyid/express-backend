3567a15bfca03bfd88b4949ee69d5427293908f4
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-09-23 16:51:27 +0700

    add file filter: only image .png, .jpg, .jpeg that can be upload to server

--- src/helpers/upload.js
+++ src/helpers/upload.js
@@ -14,5 +14,14 @@ const storage = multer.diskStorage({
 
 module.exports = multer({
   storage,
+  fileFilter: function (request, file, cb) {
+    let ext = file.originalname.split('.')
+    ext = ext[ext.length - 1]
+
+    if (ext !== 'png' && ext !== 'jpg' && ext !== 'jpeg') {
+      return cb(new Error('Only images (.png, .jpg, .jpeg) are allowed'))
+    }
+    cb(null, true)
+  },
   limits: { fileSize: 500000 }
 })
