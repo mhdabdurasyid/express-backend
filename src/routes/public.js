87297adfbf312e43d1c5c62a346b96535021b0b5
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-09-28 07:13:46 +0700

    add public route for seller register

--- src/routes/public.js
+++ src/routes/public.js
@@ -1,11 +1,13 @@
 const { Router } = require('express')
 const { getDetailItem, getItems } = require('../controller/items')
 const { addCostumer } = require('../controller/costumers')
+const { addSeller } = require('../controller/sellers')
 
 const router = Router()
 
 router.get('/item/', getItems)
 router.get('/item/:id', getDetailItem)
 router.post('/costumer/', addCostumer)
+router.post('/seller/', addSeller)
 
 module.exports = router
--- src/routes/sellers.js
+++ src/routes/sellers.js
@@ -1,11 +1,10 @@
 const { Router } = require('express')
-const { getSellers, addSeller, updateSeller, updateSellerPartial, deleteSeller, getDetailSeller } = require('../controller/sellers')
+const { getSellers, updateSeller, updateSellerPartial, deleteSeller, getDetailSeller } = require('../controller/sellers')
 
 const router = Router()
 
 router.get('/', getSellers)
 router.get('/:id', getDetailSeller)
-router.post('/', addSeller)
 router.put('/:id', updateSeller)
 router.patch('/:id', updateSellerPartial)
 router.delete('/:id', deleteSeller)
