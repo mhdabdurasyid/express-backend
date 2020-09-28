6e8c4b123104f8a387463ac7903acf2b15586787
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-09-28 07:08:43 +0700

    add public route for costumer register

--- src/routes/costumers.js
+++ src/routes/costumers.js
@@ -1,10 +1,9 @@
 const { Router } = require('express')
-const { addCostumer, updateCostumerPartial, deleteCostumer, getDetailCostumer } = require('../controller/costumers')
+const { updateCostumerPartial, deleteCostumer, getDetailCostumer } = require('../controller/costumers')
 
 const router = Router()
 
 router.get('/:id', getDetailCostumer)
-router.post('/', addCostumer)
 router.patch('/:id', updateCostumerPartial)
 router.delete('/:id', deleteCostumer)
 
--- src/routes/public.js
+++ src/routes/public.js
@@ -1,9 +1,11 @@
 const { Router } = require('express')
 const { getDetailItem, getItems } = require('../controller/items')
+const { addCostumer } = require('../controller/costumers')
 
 const router = Router()
 
 router.get('/item/', getItems)
 router.get('/item/:id', getDetailItem)
+router.post('/costumer/', addCostumer)
 
 module.exports = router
