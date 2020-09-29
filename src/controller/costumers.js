85acde8f3b26144ee773ff606d9b90ebeb564449
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-09-29 22:54:48 +0700

    add upload image when update costumer profile using PATCH method

--- src/controller/costumers.js
+++ src/controller/costumers.js
@@ -1,6 +1,8 @@
 const { addCostumerModel, getDetailCostumerModel, updateCostumerPartialModel, deleteCostumerModel } = require('../models/costumers')
 const responseStandard = require('../helpers/responses')
 const bcrypt = require('bcryptjs')
+const upload = require('../helpers/upload')
+const fs = require('fs')
 
 module.exports = {
   addCostumer: (request, response) => {
@@ -51,36 +53,61 @@ module.exports = {
     }
   },
   updateCostumerPartial: (request, response) => {
+    const uploadImage = upload.single('image')
     let { id } = request.params
     id = Number(id)
 
-    const { email = '', password = '', name = '', phone = '', birthday = '', genderID = '' } = request.body
-
     if (typeof id === 'number' && !isNaN(id)) {
-      if (email.trim() || password.trim() || name.trim() || phone.trim() || birthday.trim() || genderID.trim()) {
-        const patchData = Object.entries(request.body).map(el => {
-          if (el[0] === 'password') {
-            const salt = bcrypt.genSaltSync(10)
-            const hashedPassword = bcrypt.hashSync(password, salt)
-            return `${el[0]} = '${hashedPassword}'`
-          }
-          return `${el[0]} = '${el[1].replace(/'/gi, "''")}'`
-        }).join(', ')
+      getDetailCostumerModel(id, (err, res) => {
+        if (!err) {
+          if (res.length) {
+            uploadImage(request, response, (error) => {
+              if (error) {
+                return responseStandard(response, error.message, {}, 400, false)
+              } else {
+                const image = request.file
+                const { email = '', password = '', name = '', phone = '', birthday = '', gender_id = '' } = request.body
 
-        updateCostumerPartialModel(id, patchData, (error, result) => {
-          if (!error) {
-            if (result.affectedRows) {
-              return responseStandard(response, `Success update costumer with ID ${id}!`, {})
-            } else {
-              return responseStandard(response, `Update failed! ID ${id} not found`, {}, 400, false)
-            }
+                if (email.trim() || password.trim() || name.trim() || phone.trim() || birthday.trim() || gender_id.trim() || image) {
+                  const patchData = Object.entries(request.body).map(el => {
+                    if (el[0] === 'password') {
+                      const salt = bcrypt.genSaltSync(10)
+                      const hashedPassword = bcrypt.hashSync(password, salt)
+                      return `${el[0]} = '${hashedPassword}'`
+                    }
+                    return `${el[0]} = '${el[1].replace(/'/gi, "''")}'`
+                  })
+
+                  if (image) {
+                    patchData.push(`photo_profile = '/uploads/${image.filename}'`)
+                  }
+
+                  updateCostumerPartialModel(id, patchData, (error, result) => {
+                    if (!error) {
+                      if (result.affectedRows) {
+                        if (res[0].photo_profile !== '') {
+                          fs.unlinkSync(`assets/${res[0].photo_profile}`)
+                        }
+                        return responseStandard(response, `Success update costumer with ID ${id}!`, {})
+                      } else {
+                        return responseStandard(response, `Update failed! ID ${id} not found`, {}, 400, false)
+                      }
+                    } else {
+                      return responseStandard(response, error.message, {}, 500, false)
+                    }
+                  })
+                } else {
+                  return responseStandard(response, 'All field must be fill', {}, 400, false)
+                }
+              }
+            })
           } else {
-            return responseStandard(response, error.message, {}, 500, false)
+            return responseStandard(response, 'No data found', {}, 200, false)
           }
-        })
-      } else {
-        return responseStandard(response, 'All field must be fill', {}, 400, false)
-      }
+        } else {
+          return responseStandard(response, err.message, {}, 500, false)
+        }
+      })
     } else {
       return responseStandard(response, 'Invalid or bad ID', {}, 400, false)
     }
