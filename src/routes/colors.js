1bb72c64ca8bac1f3c7ebb44b4268fce68d61a7b
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-10-05 20:48:41 +0700

    change route GET '/color' to public

--- src/routes/colors.js
+++ src/routes/colors.js
@@ -1,10 +1,8 @@
 const { Router } = require('express')
-const { getColors, addColor, updateColor, deleteColor } = require('../controller/colors')
+const { addColor, updateColor, deleteColor } = require('../controller/colors')
 
 const router = Router()
 
-router.get('/', getColors)
-// router.get('/:id', getDetailColor)
 router.post('/', addColor)
 router.put('/:id', updateColor)
 router.delete('/:id', deleteColor)
--- src/routes/public.js
+++ src/routes/public.js
@@ -3,6 +3,7 @@ const { getDetailItem, getItems } = require('../controller/items')
 const { addCostumer } = require('../controller/costumers')
 const { addSeller } = require('../controller/sellers')
 const { getCategories, getDetailCategory } = require('../controller/categories')
+const { getColors } = require('../controller/colors')
 
 const router = Router()
 
@@ -12,5 +13,6 @@ router.post('/costumer/', addCostumer)
 router.post('/seller/', addSeller)
 router.get('/category', getCategories)
 router.get('/category/:id', getDetailCategory)
+router.get('/color', getColors)
 
 module.exports = router
