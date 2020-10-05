201798307d31b3fa788b2ff034ad62e9c97dfd90
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-10-05 20:51:22 +0700

    change route GET '/condition' to public

--- src/routes/conditions.js
+++ src/routes/conditions.js
@@ -1,10 +1,8 @@
 const { Router } = require('express')
-const { getConditions, addCondition, updateCondition, deleteCondition } = require('../controller/conditions')
+const { addCondition, updateCondition, deleteCondition } = require('../controller/conditions')
 
 const router = Router()
 
-router.get('/', getConditions)
-// router.get('/:id', getDetailCondition)
 router.post('/', addCondition)
 router.put('/:id', updateCondition)
 router.delete('/:id', deleteCondition)
--- src/routes/public.js
+++ src/routes/public.js
@@ -4,6 +4,7 @@ const { addCostumer } = require('../controller/costumers')
 const { addSeller } = require('../controller/sellers')
 const { getCategories, getDetailCategory } = require('../controller/categories')
 const { getColors } = require('../controller/colors')
+const { getConditions } = require('../controller/conditions')
 
 const router = Router()
 
@@ -14,5 +15,6 @@ router.post('/seller/', addSeller)
 router.get('/category', getCategories)
 router.get('/category/:id', getDetailCategory)
 router.get('/color', getColors)
+router.get('/condition', getConditions)
 
 module.exports = router
